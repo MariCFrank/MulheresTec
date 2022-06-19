9d98a1d90791b1c1ea17d97cb422af8f3eb944ca
MariCFrank <marianacfrank@gmail.com>
2022-06-19 19:36:17 +0200

    salvando projeto original

---
+++ js/script.js
@@ -0,0 +1,11 @@
+ document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
+
+function validaFormulario(){
+  if(document.getElementById("nome").value != "" && document.getElementById("email").value != "" && document.getElementById("telefone").value != ""){
+  alert("Prontinho! Você receberá as novidades por e-mail.")
+  }else{
+    alert("Ops! Preencha os campos de nome, e-mail e telefone!")
+  }
+}
+
+
